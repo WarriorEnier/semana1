b19570405d05f3f0ce6e6ed87e1521bd09c973b6
GuerreroEnier <guerreroenier@gmail.com>
2022-04-11 17:35:57 -0500

    solucion HW

--- FT-M1-master/00-IntroToCS/homework/homework.js
+++ FT-M1-master/00-IntroToCS/homework/homework.js
@@ -2,12 +2,27 @@
 
 function BinarioADecimal(num) {
   // tu codigo aca
+  let valor = 0;
+  num.split('').reverse().forEach((ele, index)=>{
+   
+    if(ele!=0){
+      valor+=2**index;
+    }
+  });
+  return valor;
 
 }
 
 function DecimalABinario(num) {
   // tu codigo aca
-
+  let arr = [];
+  while(num>0){
+    arr.push(num%2);
+    
+    num = Math.floor(num/2);
+    
+  }
+  return arr.reverse().join('');
 }
 
 
